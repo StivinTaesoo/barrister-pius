e535e3950b6c5c46e6ce28a445dc2b97e246da62
StivinTaesoo <stephentersoo17@gmail.com>
2024-03-15 14:36:39 +0100

    Worked on the Navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,18 +2,27 @@ import Link from "next/link";
 
 export const Navbar = () => {
     return (
-        <div className="w-full flex items-center justify-center bg-grey  py-4 bg-opacity-50  fixed top-4 z-20">
-            <div className="flex items-center justify-center gap-x-8 opacity-none text-black font-semibold ">
-                <Link href="/">Home</Link>
-                <Link href="/about">About</Link>
-                <Link href="/gallery">Gallery</Link>
+        <div className="nav-container w-full flex items-center justify-center bg-grey backdrop-blur-sm  py-4 bg-opacity-10  fixed top-4 z-20">
+            <ul className="flex items-center justify-center gap-x-8 opacity-none text-[22px] text-black font-semibold ">
+                <li>
+                    <Link href="/" className="relative">
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
+                <li>
+                    <Link href="/gallery">Gallery</Link>
+                </li>
+
                 <Link
                     href="/contact"
-                    className="px-4 py-2 text-white bg-bluePrimary rounded-lg shadow-2xl"
+                    className="px-4 py-2 text-white text-[24px] bg-bluePrimary rounded-lg shadow-lg shadow-slate-700/70 hover:bg-blue-300"
                 >
                     Contact Me
                 </Link>
-            </div>
+            </ul>
         </div>
     );
 };
