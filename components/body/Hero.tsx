5501e031f939c5ca5e1112da9fe908bcb01d81ee
StivinTaesoo <stephentersoo17@gmail.com>
2024-03-27 12:11:07 +0100

    Made changes

--- components/body/Hero.tsx
+++ components/body/Hero.tsx
@@ -6,10 +6,10 @@ export default function HeroSection() {
         >
             <div className="w-full h-full  bg-transGrey absolute top-0 left-0 flex flex-col items-center justify-center">
                 <div className="absolute bottom-5 text-center">
-                    <h1 className="uppercase text-3xl font-bold">
+                    <h1 className="uppercase text-3xl font-bold text-blue-400">
                         Barrister Pius Ukeyima Akutah, mon
                     </h1>
-                    <p>
+                    <p className="text-slate-500">
                         Executive Secretary/Chief Executive Officer (CEO) of the
                         Nigerian Shippers' Council
                     </p>
