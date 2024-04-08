2c58a33ce4778033ebd4db49e196bc550384f0e7
StivinTaesoo <stephentersoo17@gmail.com>
2024-04-08 14:02:21 +0100

    More changes to gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -101,6 +101,109 @@ function page() {
                     </div>
                 </div>
             </div>
+            {/* Section 3 */}
+            <div className="w-[95%] mx-auto my-[4rem] md:w-[80%] ">
+                <p className="text-center mb-2">
+                    Nigerian Shippers' Council had the honour of hosting Hon.
+                    Abdussamad Dasuki, Chairman, House Committe on Shipping
+                    Services and its Members. It was a time of engaging in
+                    productive discussions towards a thriving Blue and Marine
+                    Economy. A great wave of change is on the horizon.
+                </p>
+                <div className="grid grid-cols-1 gap-y-4 justify-items-center md:grid-cols-2">
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/hcc-1.jpeg"
+                            alt=""
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    </div>
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/hcc-2.jpeg"
+                            alt=""
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    </div>
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/hcc-3.jpeg"
+                            alt=""
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    </div>
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/hcc-4.jpeg"
+                            alt=""
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    </div>
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/hcc-5.jpeg"
+                            alt=""
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    </div>
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/hcc-6.jpeg"
+                            alt=""
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* Section 4 */}
+            <div className="w-[95%] mx-auto my-[4rem] md:w-[80%] ">
+                <p className="text-center mb-2">
+                    Barr. Akutah received the APC State Chairman, Hon. Austin
+                    Agada and other state officials at his private residence
+                </p>
+                <div className="grid grid-cols-1 gap-y-4 justify-items-center md:grid-cols-2">
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/img-6.jpg"
+                            alt=""
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    </div>
+
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/img-5.jpg"
+                            alt=""
+                            className="w-full h-full object-contain rounded-md shadow-md"
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* Section 5 */}
+            <div className="w-[95%] mx-auto my-[4rem] md:w-[80%] ">
+                <p className="text-center mb-2">
+                    APC Youth League, Comrade Faeren Solomon Atir paid a
+                    congratulatory visit to the Barr. Akutah at the NSC Head
+                    Office in Lagos.
+                </p>
+                <div className="grid grid-cols-1 gap-y-4 justify-items-center md:grid-cols-2">
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/img-8.jpg"
+                            alt=""
+                            className="w-full h-full object-cover rounded-md shadow-md"
+                        />
+                    </div>
+
+                    <div className="w-full h-[250px] rounded-md shadow-md md:w-[95%] md:h-[300px]">
+                        <img
+                            src="/images/img-9.jpg"
+                            alt=""
+                            className="w-full h-full object-contain rounded-md shadow-md"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
